5c25a4d30f4e4842cfb43343f32031c58d539c8f
Martino Mensio <martinomensio@outlook.it>
2018-11-12 12:03:24 +0000

    urls versioned for deply

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 import json
 
-def load_data(path='../datasets/data/all.json', by_url=False):
+def load_data(path='urls.json', by_url=False):
     with open(path) as f:
         content = json.load(f)
     if by_url:
