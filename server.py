d12177f39f4d3e31adc90cf26a31d7615cfab14a
Martino Mensio <martinomensio@outlook.it>
2019-01-10 12:15:40 +0000

    managing multiple twitter credentials, wrapping things

--- server.py
+++ server.py
@@ -22,13 +22,13 @@ CORS(app)
 ma = Marshmallow(app)
 #api = Api(app)
 
+twitterApi = twitter.TwitterAPI()
+
 mappings = {}
 mappings_path = 'cache/url_mappings.json'
 if os.path.isfile(mappings_path):
      with open(mappings_path) as f:
           mappings = json.load(f)
-
-bearer_token = twitter.get_bearer_token()
 
 '''
 # Marshmallow schemas
@@ -45,44 +45,49 @@ def analyse_url():
     res = data.classify_url(url)
     return jsonify({'result': res})
 
+"""
 @app.route('/tweets')
 def get_tweets_from_display_name():
     handle = request.args.get('handle')
     tweets = twitter.get_user_tweets(bearer_token, handle)
     return jsonify(tweets)
+"""
 
-@app.route('/tweets_api')
+@app.route('/tweets')
 def get_tweets_from_display_name2():
     handle = request.args.get('handle')
-    tweets = twitter.get_user_tweets_api(bearer_token, handle)
+    tweets = twitterApi.get_user_tweets(handle)
     return jsonify(tweets)
 
-@app.route('/followers_api')
+@app.route('/followers')
 def get_followers():
     handle = request.args.get('handle')
-    followers = twitter.get_followers_api(bearer_token, handle)
+    followers = twitterApi.get_followers(handle)
     return jsonify(followers)
 
-@app.route('/following_api')
+@app.route('/following')
 def get_following():
     handle = request.args.get('handle')
-    following = twitter.get_followers_api(bearer_token, handle)
+    following = twitterApi.get_followers(handle)
     return jsonify(following)
 
+"""
 @app.route('/urls')
 def get_shared_urls():
     handle = request.args.get('handle')
     tweets = twitter.get_user_tweets(bearer_token, handle)
     urls = twitter.get_urls_from_tweets(tweets, mappings)
     return jsonify(urls)
+"""
 
-@app.route('/urls_api')
-def get_shared_urls_api():
+@app.route('/urls')
+def get_shared_urls():
     handle = request.args.get('handle')
-    tweets = twitter.get_user_tweets_api(bearer_token, handle)
+    tweets = twitterApi.get_user_tweets(handle)
     urls = twitter.get_urls_from_tweets(tweets, mappings)
     return jsonify(urls)
 
+"""
 @app.route('/evaluate_old')
 @cross_origin()
 def evaluate_user():
@@ -91,16 +96,17 @@ def evaluate_user():
     urls = twitter.get_urls_from_tweets(tweets, mappings)
     result = evaluate.count(urls, tweets, handle)
     return jsonify(result)
+"""
 
 def get_tweets_wrap(handle):
-    return twitter.get_user_tweets_api(bearer_token, handle)
+    return twitterApi.get_user_tweets(handle)
 
 @app.route('/analyse/tweets')
 @cross_origin()
 def analyse_tweets():
     """from a list of tweet IDs (comma-separated) retrieves and analyses them"""
     tweet_ids = request.args.get('ids')
-    tweets = twitter.get_statuses_lookup(bearer_token, tweet_ids)
+    tweets = twitterApi.get_statuses_lookup(tweet_ids)
     urls = twitter.get_urls_from_tweets(tweets, mappings)
     result = evaluate.count(urls, tweets, None)
     return jsonify(result)
@@ -109,7 +115,7 @@ def analyse_tweets():
 @cross_origin()
 def analyse_user():
     handle = request.args.get('handle')
-    tweets = twitter.get_user_tweets_api(bearer_token, handle)
+    tweets = twitterApi.get_user_tweets(handle)
     urls = twitter.get_urls_from_tweets(tweets, mappings)
     result = evaluate.count(urls, tweets, handle)
     # evaluate also the following
@@ -117,7 +123,7 @@ def analyse_user():
     print(include_following)
     if include_following:
         result['following'] = {}
-        following = twitter.get_following_api(bearer_token, handle)
+        following = twitterApi.get_following(handle)
         print(len(following), 'following')
         with concurrent.futures.ProcessPoolExecutor() as executor:
             for f, tweets_f in zip(following, executor.map(get_tweets_wrap, following)):
@@ -125,7 +131,7 @@ def analyse_user():
                 result_f = evaluate.count(urls_f, tweets_f, handle)
                 result['following'][f] = result_f['you']
         """for f in following:
-            tweets_f = twitter.get_user_tweets_api(bearer_token, f)
+            tweets_f = twitterApi.get_user_tweets(f)
             urls_f = twitter.get_urls_from_tweets(tweets_f, mappings)
             result_f = evaluate.count(urls_f, tweets_f, handle)
             result['following'][f] = result_f['you']"""
@@ -137,7 +143,7 @@ def analyse_user_network():
     handle = request.args.get('handle')
     # evaluate the following
     following_analysis = {}
-    following = twitter.get_following_api(bearer_token, handle)
+    following = twitterApi.get_following(handle)
     print(len(following), 'following')
     with concurrent.futures.ProcessPoolExecutor() as executor:
         for f, tweets_f in zip(following, executor.map(get_tweets_wrap, following)):
@@ -145,7 +151,7 @@ def analyse_user_network():
             result_f = evaluate.count(urls_f, tweets_f, handle)
             following_analysis[f] = result_f['you']
     """for f in following:
-        tweets_f = twitter.get_user_tweets_api(bearer_token, f)
+        tweets_f = twitterApi.get_user_tweets(f)
         urls_f = twitter.get_urls_from_tweets(tweets_f, mappings)
         result_f = evaluate.count(urls_f, tweets_f, handle)
         result['following'][f] = result_f['you']"""
--- twitter.py
+++ twitter.py
@@ -12,53 +12,128 @@ from tqdm import tqdm
 
 import unshortener
 
+class TwitterAPI(object):
 
-def get_bearer_token():
-    # these env variables must be there
-    tw_key = os.environ['KEY_3']
-    tw_secret = os.environ['SECRET_3']
-    response = requests.post('https://api.twitter.com/oauth2/token', data={'grant_type': 'client_credentials'}, auth=HTTPBasicAuth(tw_key, tw_secret)).json()
-    assert response['token_type'] == 'bearer'
-    print('twitter OK')
-    return response['access_token']
+    def __init__(self):
+        # retrieve all the possible twitter API keys and put them in the .env file as TWITTER_KEY_x TWITTER_SECRET_x with x=0,1,2,3,...
 
-def get_user_tweets_api(bearer_token, user_handle):
-    headers = {'Authorization': 'Bearer {}'.format(bearer_token)}#.format(base64.b64encode(bearer_token.encode('utf-8')))}
-    params = {
-        'screen_name': user_handle,
-        'max_count': 200,
-        'tweet_mode': 'extended' # to get the full content and all the URLs
-    }
-    all_tweets = []
-    newest_saved = 0
-    cache_file = 'cache/tweets/{}.json'.format(user_handle)
-    if os.path.isfile(cache_file):
-        with open(cache_file) as f:
-            all_tweets = json.load(f)
-        newest_saved = max([t['id'] for t in all_tweets])
-    while True:
-        response = requests.get('https://api.twitter.com/1.1/statuses/user_timeline.json', headers=headers, params=params).json()
-        if 'errors' in response or 'error' in response:
-            print(response)
-            return all_tweets
+        # the set of credentials
+        self.credentials = []
+        # the set of tokens corresponding to the credentials
+        self.bearer_tokens = []
+        # which credential is being used
+        self.active = 0
+        while True:
+            key = os.environ.get('TWITTER_KEY_{}'.format(self.active), None)
+            if not key:
+                break
+            secret = os.environ['TWITTER_SECRET_{}'.format(self.active)]
+            self.credentials.append({'key': key, 'secret': secret})
+            token = self.get_bearer_token(key, secret)
+            self.bearer_tokens.append(token)
+            self.active += 1
+        if not self.bearer_tokens:
+            raise ValueError('you don\'t have twitter credentials in the environment!!!')
+        self.active = 0
 
-        print('.', end='', flush=True)
-        #print(response)
-        new_tweets = [t for t in response if t['id'] > newest_saved]
-        all_tweets.extend(new_tweets)
-        if not len(new_tweets):
-            print('done')
-            break
-        # set the maximum id allowed from the last tweet, and -1 to avoid duplicates
-        max_id = new_tweets[-1]['id'] - 1
-        params['max_id'] = max_id
-    print('retrieved', len(all_tweets), 'tweets')
-    if all_tweets:
-        with open(cache_file, 'w') as f:
-            json.dump(all_tweets, f, indent=2)
-    return all_tweets
+    def get_bearer_token(self, key, secret):
+        response = requests.post('https://api.twitter.com/oauth2/token', data={'grant_type': 'client_credentials'}, auth=HTTPBasicAuth(key, secret)).json()
+        assert response['token_type'] == 'bearer'
+        print('twitter bearer token OK')
+        return response['access_token']
 
-def get_user_tweets(bearer_token, user_handle):
+    def _check_expiration_decorator(func):
+        def magic(self, arg):
+            try:
+                return func(self, arg)
+            except Exception:
+                # try with another token
+                print('magically trying to handle exception')
+                self.active = (self.active + 1) % len(self.bearer_tokens)
+                return func(self, arg)
+        return magic
+
+    @_check_expiration_decorator
+    def get_user_tweets(self, user_handle):
+        token = self.bearer_tokens[self.active]
+        headers = {'Authorization': 'Bearer {}'.format(token)}#.format(base64.b64encode(bearer_token.encode('utf-8')))}
+        params = {
+            'screen_name': user_handle,
+            'max_count': 200,
+            'tweet_mode': 'extended' # to get the full content and all the URLs
+        }
+        all_tweets = []
+        newest_saved = 0
+        cache_file = 'cache/tweets/{}.json'.format(user_handle)
+        if os.path.isfile(cache_file):
+            with open(cache_file) as f:
+                all_tweets = json.load(f)
+            newest_saved = max([t['id'] for t in all_tweets])
+        while True:
+            response = requests.get('https://api.twitter.com/1.1/statuses/user_timeline.json', headers=headers, params=params).json()
+            if 'errors' in response or 'error' in response:
+                print(response)
+                return all_tweets
+
+            print('.', end='', flush=True)
+            #print(response)
+            new_tweets = [t for t in response if t['id'] > newest_saved]
+            all_tweets.extend(new_tweets)
+            if not len(new_tweets):
+                print('done')
+                break
+            # set the maximum id allowed from the last tweet, and -1 to avoid duplicates
+            max_id = new_tweets[-1]['id'] - 1
+            params['max_id'] = max_id
+        print('retrieved', len(all_tweets), 'tweets')
+        if all_tweets:
+            with open(cache_file, 'w') as f:
+                json.dump(all_tweets, f, indent=2)
+        return all_tweets
+
+    @_check_expiration_decorator
+    def get_followers(self, user_handle):
+        token = self.bearer_tokens[self.active]
+        headers = {'Authorization': 'Bearer {}'.format(token)}#.format(base64.b64encode(bearer_token.encode('utf-8')))}
+        params = {
+            'screen_name': user_handle,
+            'count': 20, # TODO loop over groups of 200
+            'cursor': -1
+            #'cursor': -1,
+        }
+        response = requests.get('https://api.twitter.com/1.1/followers/list.json', params=params, headers=headers).json()
+        if not 'users' in response:
+            return []
+        return [u['screen_name'] for u in response['users']]
+
+    @_check_expiration_decorator
+    def get_following(self, user_handle):
+        token = self.bearer_tokens[self.active]
+        headers = {'Authorization': 'Bearer {}'.format(token)}#.format(base64.b64encode(bearer_token.encode('utf-8')))}
+        params = {
+            'screen_name': user_handle,
+            'count': 20, # TODO loop over groups of 200
+            'cursor': -1
+            #'cursor': -1,
+        }
+        response = requests.get('https://api.twitter.com/1.1/friends/list.json', params=params, headers=headers).json()
+        print(response)
+        if not 'users' in response:
+            return []
+        return [u['screen_name'] for u in response['users']]
+
+    @_check_expiration_decorator
+    def get_statuses_lookup(self, tweet_ids):
+        token = self.bearer_tokens[self.active]
+        headers = {'Authorization': 'Bearer {}'.format(token)}#.format(base64.b64encode(bearer_token.encode('utf-8')))}
+        params = {
+            'id': tweet_ids
+        }
+        response = requests.get('https://api.twitter.com/1.1/statuses/lookup.json', params=params, headers=headers).json()
+        return response
+
+"""
+def get_user_tweets(user_handle):
     list_of_tweets = query_tweets_from_user(user_handle)
     results = [{'entities': {'urls': get_urls_scraper(t)}, 'id': int(t.id), 'id_str': t.id} for t in list_of_tweets]
     return results
@@ -69,6 +144,7 @@ def get_urls_scraper(tweet):
     #tree = etree.parse(tweet['html'], htmlparser)
     urls = [{'expanded_url': el['data-expanded-url']} for el in soup.select('a[data-expanded-url]')]
     return urls
+"""
 
 
 def get_urls_from_tweets(tweets, mappings, resolve=False):
@@ -100,38 +176,3 @@ def get_urls_from_tweets(tweets, mappings, resolve=False):
     with open('cache/url_mappings.json', 'w') as f:
         json.dump(mappings, f, indent=2)
     return all_urls
-
-def get_followers_api(bearer_token, user_handle):
-    headers = {'Authorization': 'Bearer {}'.format(bearer_token)}#.format(base64.b64encode(bearer_token.encode('utf-8')))}
-    params = {
-        'screen_name': user_handle,
-        'count': 20, # TODO loop over groups of 200
-        'cursor': -1
-        #'cursor': -1,
-    }
-    response = requests.get('https://api.twitter.com/1.1/followers/list.json', params=params, headers=headers).json()
-    if not 'users' in response:
-        return []
-    return [u['screen_name'] for u in response['users']]
-
-def get_following_api(bearer_token, user_handle):
-    headers = {'Authorization': 'Bearer {}'.format(bearer_token)}#.format(base64.b64encode(bearer_token.encode('utf-8')))}
-    params = {
-        'screen_name': user_handle,
-        'count': 20, # TODO loop over groups of 200
-        'cursor': -1
-        #'cursor': -1,
-    }
-    response = requests.get('https://api.twitter.com/1.1/friends/list.json', params=params, headers=headers).json()
-    print(response)
-    if not 'users' in response:
-        return []
-    return [u['screen_name'] for u in response['users']]
-
-def get_statuses_lookup(bearer_token, tweet_ids):
-    headers = {'Authorization': 'Bearer {}'.format(bearer_token)}#.format(base64.b64encode(bearer_token.encode('utf-8')))}
-    params = {
-        'id': tweet_ids
-    }
-    response = requests.get('https://api.twitter.com/1.1/statuses/lookup.json', params=params, headers=headers).json()
-    return response
