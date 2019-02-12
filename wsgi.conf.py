e5398ab5908497c9f1438a2b40f3c45bd8628905
Martino Mensio <martino.mensio@open.ac.uk>
2019-02-12 14:51:12 +0000

    optimize number of workers

--- wsgi.conf.py
+++ wsgi.conf.py
@@ -1,6 +1,8 @@
+import multiprocessing
 
 bind = '0.0.0.0:5000'
 timeout = 60
-worker_class = 'gevent'
-workers = 4
-threads = 8
+#worker_class = 'gevent'
+# http://docs.gunicorn.org/en/stable/design.html#how-many-workers
+workers = multiprocessing.cpu_count() * 2 + 1
+#threads = 64
