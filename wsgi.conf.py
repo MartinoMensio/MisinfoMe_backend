47afbef1adb6d96bb0c08f0fe855d13ccc1bf71b
Martino Mensio <martino.mensio@open.ac.uk>
2019-02-12 17:55:47 +0000

    using gevent

--- wsgi.conf.py
+++ wsgi.conf.py
@@ -2,7 +2,10 @@ import multiprocessing
 
 bind = '0.0.0.0:5000'
 timeout = 60
-#worker_class = 'gevent'
+# using gevent to handle multiple request for each worker using asyncIO
+worker_class = 'gevent'
 # http://docs.gunicorn.org/en/stable/design.html#how-many-workers
 workers = multiprocessing.cpu_count() * 2 + 1
-#threads = 64
+# threads = ? # this is not used by gevent
+# worker_connections is default to 1000
+worker_connections = 100
