0ebf9889524acfc213957e74fa518816248d6ed3
Martino Mensio <martino.mensio@open.ac.uk>
2019-06-20 11:09:06 +0100

    changing again the static serving paths

--- api/resources/static_resources.py
+++ api/resources/static_resources.py
@@ -1,13 +1,15 @@
 from flask import send_from_directory, redirect
 
 def configure_static_resources(base_url, app):
-    app_url = base_url + '/app'
+    #app_url = base_url + '/app'
+    app_url = base_url
 
     @app.route(base_url + '/static/<path:path>')
     def static_proxy(path):
         # the static files
         print('here in static_proxy', path)
         return send_from_directory('../app', path)
+
 
     @app.route(app_url + '/<path:path>')
     @app.route(app_url + '/')
@@ -16,11 +18,15 @@ def configure_static_resources(base_url, app):
         print('here in deep_linking')
         return send_from_directory('../app', 'index.html')
 
+    @app.route(base_url + '/favicon.ico')
+    def favicon_helper():
+        return send_from_directory('../app', 'favicon.ico')
+
     @app.route('/')
     @app.route(base_url)
     @app.route(base_url + '/')
-    @app.route(app_url)
+    #@app.route(app_url)
     def redirect_home():
         print('here in redirect_home')
         # this route is for sending the user to the homepage
-        return redirect(app_url + '/home')
+        return redirect(base_url + '/home')
