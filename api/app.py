064826a8923f8db1137693361f5871a4ad5f2664
Martino Mensio <martino.mensio@open.ac.uk>
2019-11-12 12:28:58 +0000

    added event tracking

--- api/app.py
+++ api/app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, Blueprint
+import requests
+from flask import Flask, Blueprint, request
 from flask_restplus import Api
 from flask.json import JSONEncoder
 import datetime
@@ -26,3 +27,37 @@ resources.configure_cors(app)
 dashboard.config.link = 'misinfo/api/dashboard'
 dashboard.config.password = os.environ['DASHBOARD_PASSWORD']
 dashboard.bind(app)
+
+# logging to Analytics
+logging_enabled = os.environ.get('LOGGING_ENABLED', False) == 'yes'
+@app.after_request
+def after_request(response):
+    if not logging_enabled:
+        return response
+    #print('after_request', request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    try:
+        data = {
+            'v': '1',  # API Version.
+            'tid': 'UA-143815717-1',  # Tracking ID / Property ID.
+            'cid': 666, # client identifier
+            't': 'event',  # Event hit type.
+            'ec': 'backend_call',  # Event category.
+            'ea': f'{request.method} {request.full_path}',  # Event action.
+            'el': request.full_path,  # Event label.
+            'ev': response.status_code,  # Event value, must be an integer
+            'dp': request.full_path # where the event happened
+        }
+        headers = {
+            'User-Agent': 'PostmanRuntime/7.19.0',
+            'Accept': '*'
+        }
+
+        analytics_response = requests.get(
+            'https://www.google-analytics.com/r/collect', params=data, headers=headers)
+        #print(analytics_response.url)
+        
+        #print('analytics_response', analytics_response.status_code)
+
+    except Exception as e:
+        print(e)
+    return response
